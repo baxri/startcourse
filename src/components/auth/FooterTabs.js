fa7293ccd85440f4c39a375e8972e08eeb787438
giorgi <giorgi.bibilashvili89@gmail.com>
2019-03-21 06:49:47 +0400

    Add Ripple Tabs I The Footer

--- src/components/auth/FooterTabs.js
+++ src/components/auth/FooterTabs.js
@@ -8,26 +8,26 @@ export default ({ active }) => {
     return (
         <Footer style={styles.footer}>
             <FooterTab style={styles.footerTab}>
-                <TouchableOpacity vertical style={active == 'home' ? styles.buttonActive : styles.button}
+                <Ripple vertical rippleOpacity={0.1} ippleContainerBorderRadius={0} style={active == 'home' ? styles.buttonActive : styles.button}
                     onPress={() => Actions.home()}>
                     <Icon style={active == 'home' ? styles.iconActive : styles.icon} active name='home' type="FontAwesome" />
                     <Text uppercase={false} style={active == 'home' ? styles.textActive : styles.text}>Home</Text>
-                </TouchableOpacity>
-                <TouchableOpacity vertical style={active == 'trip' ? styles.buttonActive : styles.button}
+                </Ripple>
+                <Ripple rippleOpacity={0.1} rippleContainerBorderRadius={0} vertical style={active == 'trip' ? styles.buttonActive : styles.button}
                     onPress={() => Actions.trips()}>
                     <Icon style={active == 'trip' ? styles.iconActive : styles.icon} active name='map' type="FontAwesome" />
                     <Text uppercase={false} style={active == 'trip' ? styles.textActive : styles.text}>Trips</Text>
-                </TouchableOpacity>
-                <TouchableOpacity vertical style={active == 'bids' ? styles.buttonActive : styles.button}
+                </Ripple>
+                <Ripple rrippleOpacity={0.1} ippleContainerBorderRadius={0} vertical style={active == 'bids' ? styles.buttonActive : styles.button}
                     onPress={() => Actions.bids()}>
                     <Icon style={active == 'bids' ? styles.iconActive : styles.icon} active name='attach-money' type="MaterialIcons" />
                     <Text uppercase={false} style={active == 'bids' ? styles.textActive : styles.text}>Bids</Text>
-                </TouchableOpacity>
-                <TouchableOpacity vertical style={active == 'setup' ? styles.buttonActive : styles.button}
+                </Ripple>
+                <Ripple rippleOpacity={0.1} rippleContainerBorderRadius={0} vertical style={active == 'setup' ? styles.buttonActive : styles.button}
                     onPress={() => Actions.setup()}>
                     <Icon style={active == 'setup' ? styles.iconActive : styles.icon} active name='settings' type="SimpleLineIcons" />
                     <Text uppercase={false} style={active == 'setup' ? styles.textActive : styles.text}>Setup</Text>
-                </TouchableOpacity>
+                </Ripple>
             </FooterTab>
         </Footer>
     )
@@ -54,7 +54,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         flex: 1,
         height: 70,
-        borderRadius: 0,
     },
 
     buttonActive: {
@@ -62,7 +61,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         flex: 1,
         height: 70,
-        borderRadius: 0,
     },
 
     text: {
